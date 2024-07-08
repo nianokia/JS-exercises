8622249a74b97b48aa4cf0245a8ecefd0b54c78a
Nia Wright <nw.niawright@gmail.com>
2024-07-08 19:05:49 -0400

    Finished 4 tasks in JavaScript Fundamentals: Objects Website

--- prework/4_Tasks_JavaScriptFundamentals_ Objects.js
+++ prework/4_Tasks_JavaScriptFundamentals_ Objects.js
@@ -5,7 +5,11 @@
 //    3) Add the property surname with the value Smith.
 //    4) Change the value of the name to Pete.
 //    5) Remove the property name from the object.
-
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
 
 // Task 2: Check for emptiness
 // Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
@@ -17,18 +21,27 @@
 //    schedule["8:30"] = "get up";
 //
 //    alert( isEmpty(schedule) ); // false
-
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
 
 // Task 3: Sum object properties
 // We have an object storing salaries of our team:
-//   let salaries = {
-//      John: 100,
-//      Ann: 160,
-//      Pete: 130
-//    }
+// let salaries = { John: 100, Ann: 160, Pete: 130 }
 // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 // If salaries is empty, then the result must be 0.
-
+let salaries = { John: 100, Ann: 160, Pete: 130 };
+let salaryArr = [];
+let sum = 0;
+for (let key in salaries) {
+    if (true) {
+        salaryArr.push(salaries[key]);
+        sum = salaryArr.reduce((total, salary) =>  total + salary);
+    }
+}
 
 // Task 4: Multiply numeric property values by 2
 // Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
@@ -48,5 +61,23 @@
     //    height: 600,
     //    title: "My menu"
     //  };
-// Please note that multiplyNumeric does not need to return anything. It should modigy the object in-place.
+// Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
 // P.S. Use typeof to check for a number here.
+function multiplyNumeric(obj) {
+    if (typeof obj === "object") {
+        for (let key in obj) {
+            if (typeof obj[key] === "number") {
+                obj[key] *= 2;
+            }
+        }
+    }
+}
+
+/*
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+multiplyNumeric(menu); */
